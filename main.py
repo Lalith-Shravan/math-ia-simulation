9403f529b6c0b635adcd3dda6b0787612aa93d3c
Lalith Shravan Guruprasad <lalithg@outlook.com>
2025-06-21 16:36:58 -0700

    3d Graph

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hello world!")
+import matplotlib.pyplot as plt
+from orbitalobject import OrbitalObject
+
+fig = plt.figure(figsize=(10,10))
+ax = fig.add_subplot(111, projection='3d')
+ax.set_aspect("equal")
+
+plt.show()
