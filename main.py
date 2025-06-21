9f108ec0d3e15be97488f4f76f05f320b3256c02
Lalith Shravan Guruprasad <lalithg@outlook.com>
2025-06-21 16:41:11 -0700

    Orbital Objects Created

--- main.py
+++ main.py
@@ -15,4 +15,11 @@ sphere_y = sphere_radius * np.sin(u) * np.sin(v)
 sphere_z = sphere_radius * np.cos(v)
 ax.plot_wireframe(sphere_x, sphere_y, sphere_z, color="gray")
 
+earth_mass = 5.972 * pow(10, 24)
+orbital_object_1 = OrbitalObject(earth_mass, 7000000, 0, 0)
+orbital_object_2 = OrbitalObject(earth_mass, 7000000, pi / 2, 0)
+
+object_1_plot = ax.scatter(orbital_object_1.point.x, orbital_object_1.point.y, orbital_object_1.point.z, label="Object 1", color="blue")
+object_2_plot = ax.scatter(orbital_object_2.point.x, orbital_object_2.point.y, orbital_object_2.point.z, label="Object 2", color="green")
+
 plt.show()
