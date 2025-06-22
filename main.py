ee4e18e8167d7b787ef022ceb82c824badcb3b11
Lalith Shravan Guruprasad <lalithg@outlook.com>
2025-06-21 17:06:36 -0700

    Distance and Orbit Lines Added

--- main.py
+++ main.py
@@ -19,14 +19,42 @@ earth_mass = 5.972 * pow(10, 24)
 orbital_object_1 = OrbitalObject(earth_mass, 7000000, 0, 0)
 orbital_object_2 = OrbitalObject(earth_mass, 7000000, pi / 2, 0)
 
-for simulation_time in range(0, 6000, 10):
+orbit_path_1 = [[], [], []] # x-values, y-values, z-values
+orbit_path_2 = [[], [], []] # x-values, y-values, z-values
+
+for simulation_time in range(0, 6000, 100):
     orbital_object_1.move(simulation_time)
     orbital_object_2.move(simulation_time)
 
     object_1_plot = ax.scatter(orbital_object_1.point.x, orbital_object_1.point.y, orbital_object_1.point.z, label="Object 1", color="blue")
     object_2_plot = ax.scatter(orbital_object_2.point.x, orbital_object_2.point.y, orbital_object_2.point.z, label="Object 2", color="green")
 
+
+    connection_x = [orbital_object_1.point.x, orbital_object_2.point.x]
+    connection_y = [orbital_object_1.point.y, orbital_object_2.point.y]
+    connection_z = [orbital_object_1.point.z, orbital_object_2.point.z]
+    connection_line = ax.plot(connection_x, connection_y, connection_z, color="red")
+
+    orbit_path_1[0].append(orbital_object_1.point.x)
+    orbit_path_1[1].append(orbital_object_1.point.y)
+    orbit_path_1[2].append(orbital_object_1.point.z)
+    
+    orbit_path_2[0].append(orbital_object_2.point.x)
+    orbit_path_2[1].append(orbital_object_2.point.y)
+    orbit_path_2[2].append(orbital_object_2.point.z)
+
+    orbit_path_1_plot = ax.plot(orbit_path_1[0], orbit_path_1[1], orbit_path_1[2], color="blue")
+    orbit_path_2_plot = ax.plot(orbit_path_2[0], orbit_path_2[1], orbit_path_2[2], color="green")
+
     plt.pause(0.01)
 
     object_1_plot.remove()
     object_2_plot.remove()
+    for line in connection_line:
+        line.remove()
+    
+    for line in orbit_path_1_plot:
+        line.remove()
+    
+    for line in orbit_path_2_plot:
+        line.remove()
